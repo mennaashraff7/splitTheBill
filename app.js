2d21b71747effd6525047036cad43050ba6f6400
mennaashraff7 <39194503+mennaashraff7@users.noreply.github.com>
2020-10-11 21:08:21 +0200

    second commit

--- app.js
+++ app.js
@@ -1,35 +1,22 @@
 /*Globale console,alert,prompt*/
 const person1 = document.getElementById("person1");
-const addItems = document.querySelectorAll(".btn-add");
-const submits = document.querySelectorAll(".btn");
+
+
 var i=0;
-createContent();
+
 
 function addInput(){
     'use strict'
-    var newinput = document.createElement("input"); 
-    /******* our Main problem here *********/
-    document.querySelector(".inputs").appendChild(newinput);
-    /******* end Main problem here *********/
-    
-    /* document.querySelector(".inputs")[i].appendChild(newinput);
-       we should extract index that we click on it
-        
-        byebyeeeeeee die nw 
-    
-    */
-    
-    
-    newinput.classList.add("input");
-    newinput.setAttribute("type","text");
-    newinput.setAttribute("placeholder","amount");
- 
+  
+  
     
 }
 
-function add(){
+function add(e){
     var sum=0;
+     console.log(this);
     const inputs=document.querySelectorAll(`.input`);
+    
     inputs.forEach(input =>sum += parseInt( input.value ));
     console.log(sum);
 
@@ -52,9 +39,13 @@ var numberOfPpl= prompt("Please Enter a Number","ex:1,2,3,..etc");
        label.textContent = "name:"
        var btn1 = document.createElement("button");
        btn1.classList.add("btn-add");
+       btn1.id=`btn${i+2}`;
        btn1.textContent = "+";
+        
        var div4 = document.createElement("div");
        div4.classList.add("inputs");
+        div4.id=`inp${i+2}`;
+        div4.setAttribute("name",`bbtn${i+2}`)
        var input =  document.createElement("input");
         input.classList.add("input");
         input.setAttribute("type","text");
@@ -63,20 +54,44 @@ var numberOfPpl= prompt("Please Enter a Number","ex:1,2,3,..etc");
         var btn2 = document.createElement("button");
          btn2.classList.add("btn");
          btn2.textContent = "submit";
+        btn2.id = `${i+2}`;
         div3.appendChild(label);
         div3.appendChild(btn1);
        div3.appendChild(div4);
        div3.appendChild(btn2);
           }
 }
- submits.forEach(submit => submit.addEventListener('click',add));
+
+createContent();
+
+const submits = document.querySelectorAll(".btn");
+const addItems = document.querySelectorAll(".btn-add");
+
+ submits.forEach(submit=> submit.addEventListener('click' ,function(e){
+     
+       var sum=0;
+    const inputs=document.getElementById(`inp${this.id}`);
+       
+      const children= inputs.querySelectorAll(`.input`); 
+       
+       children.forEach(child=> sum += parseInt(child.value));
+       
+          console.log(sum);
+   }));
+     
+ addItems.forEach(addItem=>addItem.addEventListener('click',function(e){
+            inputs=document.querySelector(`[name*="b${this.id}"]`);
+
+               var newinput = document.createElement("input"); 
+
+                inputs.appendChild(newinput);
+
+            newinput.classList.add("input");
+            newinput.setAttribute("type","text");
+            newinput.setAttribute("placeholder","amount");
+
+     
+     
+     
+ }))
              
-for (var i=0;i<addItems.length;i++){
-      addItems[i].addEventListener('click',addInput);
-    
-    
-    
-    
-    
-    
-  }
