71e3999f5d4ac859009be6404703068b5860566c
mennaashraff7 <39194503+mennaashraff7@users.noreply.github.com>
2020-10-12 22:16:39 +0200

    3rd commit

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
 /*Globale console,alert,prompt*/
 const person1 = document.getElementById("person1");
+const taxes = document.getElementById("tax");
+const service = document.getElementById("service");
+const button = document.getElementById("Button");
+var   totalService=0,
+        split =0;
 
 
-var i=0;
+window.onload = function(){
+    
+const submits = document.querySelectorAll(".btn");
+const addItems = document.querySelectorAll(".btn-add");
+    
+    
+submits.forEach(submit=> submit.addEventListener('click' ,function(e){
+     
+       var sum=0;
+    const inputs=document.getElementById(`inp${this.id}`);
+    const sumBox =document.getElementById(`sum${this.id}`);
+       
+      const children= inputs.querySelectorAll(`.input`); 
+       
+       children.forEach(child=> sum += parseInt(child.value));
+       
+         sumBox.textContent = "total sum:  " + sum + "L.E" +`\nTaxes:${taxes.value/numberOfPpl}\nService:${service.value/numberOfPpl}\ntotal money you should pay:${sum+split}`;
+   
+   }));
+     
+ addItems.forEach(addItem=>addItem.addEventListener('click',function(e){
+            inputs=document.querySelector(`[name*="b${this.id}"]`);
+
+               var newinput = document.createElement("input"); 
+
+                inputs.appendChild(newinput);
+
+            newinput.classList.add("input");
+            newinput.setAttribute("type","text");
+            newinput.setAttribute("placeholder","amount");
+
+     
+     
+     
+ }));
+    
+    
+    
+    
+}
+
+var i=0,
+   
+    numberOfPpl=0;
 
 
 function addInput(){
@@ -23,9 +71,17 @@ function add(e){
 }
 function createContent(){
     
-var numberOfPpl= prompt("Please Enter a Number","ex:1,2,3,..etc");
-        var div = document.querySelector('.container');
+ numberOfPpl= prompt("Please Enter a Number","ex:1,2,3,..etc");
+
+
+ 
+    
+var div = document.querySelector('.container');
+    
+    
     for(i;i<numberOfPpl;i++){
+        
+        var name = prompt("please Enter a Name ");
        
        var div2 = document.createElement("div");
        div2.classList.add("bill");
@@ -36,16 +92,28 @@ var numberOfPpl= prompt("Please Enter a Number","ex:1,2,3,..etc");
        var label =  document.createElement('label');
        label.id="person1";
        label.setAttribute("for","amount1");
-       label.textContent = "name:"
+       label.textContent = "name:";
+       
+       var span = document.createElement("span");
+       span.textContent= name;    
        var btn1 = document.createElement("button");
        btn1.classList.add("btn-add");
-       btn1.id=`btn${i+2}`;
+       btn1.id=`btn${i}`;
        btn1.textContent = "+";
+        
+        var divBox = document.createElement("div");
+        divBox.classList.add("box");
         
        var div4 = document.createElement("div");
        div4.classList.add("inputs");
-        div4.id=`inp${i+2}`;
-        div4.setAttribute("name",`bbtn${i+2}`)
+        div4.id=`inp${i}`;
+        div4.setAttribute("name",`bbtn${i}`);
+        
+       var div5 = document.createElement("div");
+       div5.classList.add("sumBox");
+        div5.id=`sum${i}`;
+      
+        
        var input =  document.createElement("input");
         input.classList.add("input");
         input.setAttribute("type","text");
@@ -54,44 +122,29 @@ var numberOfPpl= prompt("Please Enter a Number","ex:1,2,3,..etc");
         var btn2 = document.createElement("button");
          btn2.classList.add("btn");
          btn2.textContent = "submit";
-        btn2.id = `${i+2}`;
+        btn2.id = `${i}`;
+        
         div3.appendChild(label);
+        div3.appendChild(span);
         div3.appendChild(btn1);
-       div3.appendChild(div4);
-       div3.appendChild(btn2);
+        div3.appendChild(divBox);
+        divBox.appendChild(div4);
+        divBox.appendChild(div5);
+        div3.appendChild(btn2);
+    
           }
 }
 
 createContent();
-
-const submits = document.querySelectorAll(".btn");
-const addItems = document.querySelectorAll(".btn-add");
-
- submits.forEach(submit=> submit.addEventListener('click' ,function(e){
-     
-       var sum=0;
-    const inputs=document.getElementById(`inp${this.id}`);
-       
-      const children= inputs.querySelectorAll(`.input`); 
-       
-       children.forEach(child=> sum += parseInt(child.value));
-       
-          console.log(sum);
-   }));
-     
- addItems.forEach(addItem=>addItem.addEventListener('click',function(e){
-            inputs=document.querySelector(`[name*="b${this.id}"]`);
-
-               var newinput = document.createElement("input"); 
-
-                inputs.appendChild(newinput);
-
-            newinput.classList.add("input");
-            newinput.setAttribute("type","text");
-            newinput.setAttribute("placeholder","amount");
-
-     
-     
-     
- }))
+ button.addEventListener('click',function(e){
+  if(taxes.value==  " " || services.value ==" ") alert("you should fill all boxes");
+  else{ totalService = parseInt(taxes.value) + parseInt(service.value);
+      
+        split= totalService/numberOfPpl;
+      
+      
+      }
+      
+ });
+ 
              
